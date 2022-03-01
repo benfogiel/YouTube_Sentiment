fixed pandas overflow issue

scrape.py
@@ -1,24 +1,14 @@
-""""
-scrapes YouTube comments
-https://developers.google.com/youtube/v3/docs/commentThreads/list
-"""
-
-# -*- coding: utf-8 -*-
-
-# Sample Python code for youtube.commentThreads.list
-# See instructions for running these code samples locally:
-# https://developers.google.com/explorer-help/code-samples#python
-
 import os
 import googleapiclient.discovery
 import pandas as pd
+import numpy as np
 
 def get_comments(videos, file_name, comments_per_vid):
     """
     :param videos: (str) path to txt containing all the video IDs
     :param file_name: (str) file name to store comment data
     :param comments_per_vid: (int) max number of comments to scrape per video
-    :return: writes comments to csv <file_name>
+    :return: (DataFrame) headers: video_id, comment, comment_date, and sent_score (Note: sent_score vals are Nan's)
     """
 
     # get all video ids
@@ -32,7 +22,17 @@ def get_comments(videos, file_name, comments_per_vid):
     snippets = []
     for video in video_ids:
         snippets.append(scrape(video_id=video, count=comments_per_vid))
-    write(file_name=file_name,snippets=snippets)
+
+    data = {'video_id': [], 'comment': [], 'comment_date': [], 'sent_score': []}
+    for snippet in snippets:
+        for i, k in enumerate(snippet['items']):
+            data['video_id'].append(k['snippet']['videoId'])
+            data['comment'].append(k['snippet']['topLevelComment']['snippet']['textOriginal'].replace('\n', ' '))
+            data['comment_date'].append(k['snippet']['topLevelComment']['snippet']['publishedAt'])
+    # add Nan values for sent_score - these values will be added later
+    data['sent_score'] = np.full(len(data['video_id']), np.nan)
+
+    return pd.DataFrame(data)
 
 
 def scrape(video_id, count):
@@ -60,23 +60,3 @@ def scrape(video_id, count):
         videoId = video_id,
     )
     return request.execute()
-
-def write(file_name, snippets):
-    """
-    write to csv
-    param file_name: (str) csv file name
-    param snippets: (list of json objs) google comments extractor snippets
-    """
-    # merge all of them into a dictionary
-    data = {'video_id': [], 'comment': [], 'comment_date': []}
-    for snippet in snippets:
-        for i, k in enumerate(snippet['items']):
-            data['video_id'].append(k['snippet']['videoId'])
-            data['comment'].append(k['snippet']['topLevelComment']['snippet']['textOriginal'].replace('\n',' '))
-            data['comment_date'].append(k['snippet']['topLevelComment']['snippet']['publishedAt'])
-
-    df = pd.DataFrame(data)
-    df.to_csv(file_name, encoding='utf-8', index=False)
-
-if __name__ == "__main__":
-    main()